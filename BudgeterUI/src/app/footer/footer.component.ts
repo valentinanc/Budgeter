7706c61895d36d3d7354c81ddb63eef086786219
Vidur <vidurmenezes@gmail.com>
2020-10-31 19:40:49 -0400

    fixed spacing, fixed the footer and made changes for font and added icons

--- BudgeterUI/src/app/footer/footer.component.ts
+++ BudgeterUI/src/app/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'footer',
+  selector: 'Footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
