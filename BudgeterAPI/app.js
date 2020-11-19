3f4146511cb7509e5f6eda6a0b5e62a5c342da9f
Vidur <vidurmenezes@gmail.com>
2020-11-19 16:02:49 -0500

    added my changes and edited name for header

--- BudgeterAPI/app.js
+++ BudgeterAPI/app.js
@@ -51,7 +51,7 @@ app.use('/api/user-profile', userProfile);
 
 const db = require("./config/db.initialize.js");
 
-// DROPS ALL TABLE RECORDS & RECREATES TABLES
+//DROPS ALL TABLE RECORDS & RECREATES TABLES
 // db.sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
 // .then(function(){
 //     return db.sequelize.sync({ force: true });
