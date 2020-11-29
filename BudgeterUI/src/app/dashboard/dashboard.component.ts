792f6ac3188590048c861bca70fe2af11975816d
Kevin Subhash <kevinsubhash@gmail.com>
2020-11-28 20:17:34 -0500

    minor bug fix

--- BudgeterUI/src/app/dashboard/dashboard.component.ts
+++ BudgeterUI/src/app/dashboard/dashboard.component.ts
@@ -39,7 +39,6 @@ export class DashboardComponent implements OnInit {
 	guid: string;
 	constructor(private http: HttpClient, private route: ActivatedRoute, private matDialog: MatDialog, private sharedService:SharedService) {
 		this.uid = this.route.url["value"][1]["path"];
-		this.bv = false;
 		let id = this.route.url["value"][1]["path"];
 		this.clickEventsubscription = this.sharedService.getClickEvent().subscribe(()=>{
 			this.http.get('/api/user-profile/getProfile/' + id).subscribe((data:any) => {
