9586bb56057d0244910151aa16311b315890b2ee
Valentin Anculete <valentin.anculete@mail.utoronto.ca>
2020-12-07 14:58:14 -0500

    quick fix

--- BudgeterUI/src/app/dashboard/dashboard.component.ts
+++ BudgeterUI/src/app/dashboard/dashboard.component.ts
@@ -41,7 +41,6 @@ export class DashboardComponent implements OnInit {
 		this.uid = this.route.url["value"][1]["path"];
 		let id = this.route.url["value"][1]["path"];
 		this.clickEventsubscription = this.sharedService.getClickEvent().subscribe(()=>{
-			this.ngOnInit();
 			this.http.get('/api/user-profile/getProfile/' + id).subscribe((data:any) => {
 				this.tBudget = data.MBudget;
 				this.mExpenses = data.MExpenses;
